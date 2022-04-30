332404f94de87308c64958dac65d9d5a0d6cc344
Richard Cheng <rc38@princeton.edu>
2022-04-30 11:09:13 -0400

    Fixed boost hitboxes

--- src/components/objects/Turbine/Turbine.js
+++ src/components/objects/Turbine/Turbine.js
@@ -11,6 +11,7 @@ class Turbine extends Group {
         const loader = new GLTFLoader();
 
         this.name = 'turbine';
+        // this.hitbox = null;
         this.state = {
             parent: parent,
         };
@@ -19,18 +20,26 @@ class Turbine extends Group {
         parent.state.parent.state.parent.addToUpdateList(this);
 
         loader.load(MODEL, (gltf) => {
-            const scale = 10;
+            const scale = 12;
+            gltf.scene.position.x = 0;
+            gltf.scene.position.y = 0;
+            gltf.scene.position.z = 0;
+        
             gltf.scene.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2)
             gltf.scene.rotateOnAxis(new THREE.Vector3(0, 0, 1), Math.PI / 2)
+            gltf.scene.position.add(new THREE.Vector3(.83, .65, -2).multiplyScalar(scale));
             gltf.scene.scale.multiplyScalar(scale);
+            this.add(gltf.scene);
+           
             this.box = new THREE.Box3().setFromObject(gltf.scene);
 
-            this.add(gltf.scene);
+            // this.add(gltf.scene);
         });
     }
 
     update(timeStamp) {
         // this.rotation.y += 0.01
+
 
     }
 }
